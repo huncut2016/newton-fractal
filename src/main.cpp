0fa96fcb2522eae17f9231b1a2d294fd7a98594b
huncut2016 <zoller.andras2005@gmail.com>
2021-11-14 23:18:46 +0100

    hotfix

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@ int main( ){
 	//ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 
   	ofGLWindowSettings settings;
-  	settings.setGLVersion(4, 1);
+  	settings.setGLVersion(3, 2);
   	ofCreateWindow(settings);
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
